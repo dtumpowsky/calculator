124149317903eec8e45d38218d2a30726dbd3a01
David Tumpowsky and Jim Palowski <david@>
2018-03-21 13:40:53 -0700

    display correct jquery logic and comments of previous drafts

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,98 @@
+//Business (back-end) logic:
+
 var add = function(number1, number2){
 	return number1 + number2;
-}
+};
 
 var subtract = function(number1, number2){
 	return number1 - number2;
-}
+};
 
 var multiply = function(number1, number2){
 	return number1 * number2;
-}
+};
 
 var divide = function(number1, number2){
 	return number1 / number2;
-}
+};
 
-var number1 = parseInt(prompt("Enter number"));
-var number2 = parseInt(prompt("Enter another number"));
+// Front-end
 
-alert(add(number1, number2));
+$(document).ready(function() {
+  $("button#add1").click(function(event) {
+		var number1 = parseInt($("#numberChoice1").val());
+		var number2 = parseInt($("#numberChoice2").val());
+		var result = add(number1, number2);
+    $("#submit").text(result);
+		event.preventDefault();
+
+	});
+
+
+	$("button#sub1").click(function(event) {
+		var number1 = parseInt($("#numberChoice1").val());
+		var number2 = parseInt($("#numberChoice2").val());
+		var result = subtract(number1, number2);
+		$("#submit").text(result);
+		event.preventDefault();
+
+	});
+
+
+	$("button#mult1").click(function(event) {
+		var number1 = parseInt($("#numberChoice1").val());
+		var number2 = parseInt($("#numberChoice2").val());
+		var result = multiply(number1, number2);
+		$("#submit").text(result);
+		event.preventDefault();
+
+	});
+
+	$("button#divide1").click(function(event) {
+		var number1 = parseInt($("#numberChoice1").val());
+		var number2 = parseInt($("#numberChoice2").val());
+		var result = divide(number1, number2);
+		$("#submit").text(result);
+		event.preventDefault();
+
+	});
+
+	/*
+	$("form#math").submit(function(event) {
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+    $("#output").text(result);
+		event.preventDefault();
+	});
+	$("form#math").submit(function(event) {
+		var number1 = parseInt($("#sub1").val());
+		var number2 = parseInt($("#sub2").val());
+		var result = subtract(number1, number2);
+    $("#output").text(result);
+		event.preventDefault();
+  });
+	$("form#math").submit(function(event) {
+		var number1 = parseInt($("#mult1").val());
+		var number2 = parseInt($("#mult2").val());
+		var result = multiply(number1, number2);
+		$("#output").text(result);
+		event.preventDefault();
+	});
+	$("form#math").submit(function(event) {
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var result = divide(number1, number2);
+		$("#output").text(result);
+		event.preventDefault();
+	});*/
+});
 
 
 
 
 
-
-
-
-//BMI Calculator
+/*BMI Calculator
 
 var bmi = function(weight, height){
 	return (weight / (height^2));
@@ -128,4 +194,4 @@ var pintToGallon = function(inputVolume) {
 	return (inputVolume * 0.125);
 }
 
-alert(pintToGallon(pint));
+alert(pintToGallon(pint));*/
